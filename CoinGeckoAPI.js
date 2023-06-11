cf15da0fc6e656bc3d26bc1879a1f2eee146eef4
venturaadrian888 <venturaadrian888@gmail.com>
2023-06-11 22:03:37 +0800

    CAN VIEW CYPTOCOINS

--- CoinGeckoAPI.js
+++ CoinGeckoAPI.js
@@ -1,23 +1,65 @@
 import axios from 'axios';
+import moment from 'moment';
 
-const API_BASE_URL = 'https://api.coingecko.com/api/v3/coins/list';
+// const API_BASE_URL = 'https://api.coingecko.com/api/v3/coins/list';
 
-export const getCoinList = async () => {
+// export const getCoinList = async () => {
+//   try {
+//     const response = await axios.get(`${API_BASE_URL}/coins/list`);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error while fetching coin list:', error);
+//     throw error;
+//   }
+// };
+
+// export const getCoinPrice = async (coinId) => {
+//   try {
+//     const response = await axios.get(`${API_BASE_URL}/simple/price?ids=${coinId}&vs_currencies=usd`);
+//     return response.data[coinId].usd;
+//   } catch (error) {
+//     console.error(`Error while fetching price for ${coinId}:`, error);
+//     throw error;
+//   }
+// };
+
+const formatSparkline = (numbers) => {
+  const sevenDaysAgo = moment().subtract(7, 'days').unix();
+  let formattedSparkline = numbers.map((item, index) => {
+    return {
+      x: sevenDaysAgo + (index + 1) * 3600,
+      y: item,
+    }
+  })
+
+  return formattedSparkline;
+}
+
+const formatMarketData = (data) => {
+  let formattedData = [];
+
+  data.forEach(item => {
+    const formattedSparkline = formatSparkline(item.sparkline_in_7d.price)
+
+    const formattedItem = {
+      ...item,
+      sparkline_in_7d: {
+        price: formattedSparkline
+      }
+    }
+
+    formattedData.push(formattedItem);
+  });
+
+  return formattedData;
+}
+export const getMarketData = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/coins/list`);
-    return response.data;
+    const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d");
+    const data = response.data;
+    const formattedResponse = formatMarketData(data);
+    return formattedResponse;
   } catch (error) {
-    console.error('Error while fetching coin list:', error);
-    throw error;
+    console.log(error.message);
   }
-};
-
-export const getCoinPrice = async (coinId) => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/simple/price?ids=${coinId}&vs_currencies=usd`);
-    return response.data[coinId].usd;
-  } catch (error) {
-    console.error(`Error while fetching price for ${coinId}:`, error);
-    throw error;
-  }
-};
+}
--- screens/CoinListScreen.js
+++ screens/CoinListScreen.js
@@ -1,33 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import { getCoinList } from '../CoinGeckoAPI';
+import { getCoinList, getMarketData } from '../CoinGeckoAPI';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ListItem from '../components/ListItem';
+import { FlatList } from 'react-native-web';
+
+
 
 const CoinListScreen = () => {
-  const [coinList, setCoinList] = useState([]);
+  // const [coinList, setCoinList] = useState([]);
+
+  // useEffect(() => {
+  //   fetchCoinList();
+  // }, []);
+
+  // const fetchCoinList = async () => {
+  //   try {
+  //     const data = await getCoinList();
+  //     console.log(data); // Check the API response in the console
+  //     setCoinList(data);
+  //   } catch (error) {
+  //     console.error('Error fetching coin list:', error);
+  //   }
+  // };
+  
+  const  [data, setData ] = useState([]);
+  const [selectedCoinData, setSelectedCoinData] = useState(null);
 
   useEffect(() => {
-    fetchCoinList();
-  }, []);
-
-  const fetchCoinList = async () => {
-    try {
-      const data = await getCoinList();
-      console.log(data); // Check the API response in the console
-      setCoinList(data);
-    } catch (error) {
-      console.error('Error fetching coin list:', error);
+    const fetchMarketData = async () => {
+      const marketData = await getMarketData();
+      setData(marketData);
     }
-  };
-  
+
+    fetchMarketData();
+  }, [])
 
   return (
-    <View>
-      <Text>Coin List:</Text>
-      {coinList.map((coin) => (
-        <Text key={coin.id}>{coin.name}</Text>
-      ))}
-      <Text>coinList: {JSON.stringify(coinList)}</Text>
-    </View>
+    // <View>
+    //   <Text>Coin List:</Text>
+    //   {coinList.map((coin) => (
+    //     <Text key={coin.id}>{coin.name}</Text>
+    //   ))}
+    //   <Text>coinList: {JSON.stringify(coinList)}</Text>
+    // </View>
+    <SafeAreaView >
+    <FlatList
+      keyExtractor={(item) => item.id}
+      data={data}
+      renderItem={({ item }) => (
+        <ListItem
+          name={item.name}
+          symbol={item.symbol}
+          currentPrice={item.current_price}
+          priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+          logoUrl={item.image}
+          onPress={() => openModal(item)}
+        />
+      )}
+      
+    />
+    </SafeAreaView>
+    
   );
   
 };
